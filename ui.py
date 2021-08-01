948e7e83478501caa3149cc93be42322a056d370
yiming0325 <yym960325@gmail.com>
2021-08-01 18:15:04 +0100

    make some adjustments

--- ui.py
+++ ui.py
@@ -3,38 +3,11 @@ import pickle
 from treeautomaton import *
 import tkMessageBox
 import easygui
+from basedesk import basedesk
 from normal_face import normal_face
 from max_face import max_face
 
 
-class basedesk():
-    def __init__(self,master):
-        self.root = master
-        self.root.resizable(0,0)
-        self.root.title('Welcome to Tree Automata')
-
-        # welcome image
-        self.canvas = tk.Canvas(self.root, height=200, width=450)
-        self.image_file = tk.PhotoImage(file='welcome.gif')
-        self.image = self.canvas.create_image(0, 0, anchor='nw', image=self.image_file)
-        self.create_base()
-
-        self.face= normal_face(self.root)
-
-    def create_base(self):
-        self.canvas.pack(side='top')
-
-    
-    def normal_change(self):
-        self.face.destroy()
-        self.face = normal_face(self.root)
-
-    def max_change(self):
-        self.face.destroy()
-        self.face = max_face(self.root)
-
-    def clear_all(self):
-        self.face.clear_all()
 
 root = tk.Tk()
 base = basedesk(root)
